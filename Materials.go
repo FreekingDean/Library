6c31a5d91d523493731e94c1d02515b8cff99a27
Matthew Martorana <mmartorana07@gmail.com>
2014-04-24 12:02:13 -0400

    its RecMat, not

--- Materials.go
+++ Materials.go
@@ -4,7 +4,7 @@ import (
   "log"
 )
 
-func RecMatt(command string, params map[string]string) bool {
+func RecMat(command string, params map[string]string) bool {
   switch(cmd) {
     case: "recieving" { return recieve(params, client) }
     case: "purchase" { return purchase(params, client) }
